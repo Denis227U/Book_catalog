287a99ca037965a0367bef68c247565b5652cbcf
Denis Yunyaev <music_is_my_extazy@mail.ru>
2020-08-16 01:49:17 +0300

    created listPage with responsive, and added header-scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,79 +1,27 @@
 import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
-import Handlebars from 'handlebars';
+// import {
+//     menuScroll
+// } from 'menu';
 
 
-document.addEventListener("DOMContentLoaded", coreFunction);
+document.addEventListener("DOMContentLoaded", init);
 
 
-function coreFunction() {
-
-    function init() {
-
-        console.log("Пасхалка для самых любопытных =) 2020");
-        mainEvents();
-
-    }
-
-    init();
-
-    function mainEvents() {
-        console.log("Вешает все обработчики");
-    }
-
+function init() {
+    menuScroll();
 }
 
 
+function menuScroll() {
+    window.addEventListener('scroll', function () {
+        let navHeight = window.pageYOffset;
 
-
-//  --==:: HELPERS ::==-
-function sendRequest(url, callback, form, method = "GET") {
-    console.log("Отправляем запрос...");
-    let FORM_DATA;
-    // 1. Создаём новый объект XMLHttpRequest
-    const xhr = new XMLHttpRequest();
-
-    // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-    if (method == "GET") {
-        xhr.open('GET', url, true);
-        xhr.send(); // 3. Отсылаем запрос
-    } else {
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //Передает правильный заголовок в запросе
-        FORM_DATA = jQuery(form).serialize();
-        xhr.send(FORM_DATA); // 3. Отсылаем запрос
-    }
-
-    // 4. Если код ответа сервера не 200, то это ошибка
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            form ? callback(xhr.responseText, form) : callback(xhr.responseText); // вызвать колбэк и передать ему ответ
+        if (navHeight > 1) {
+            document.querySelector('.header-scroll').classList.add('fixed-menu')
+        } else {
+            document.querySelector('.header-scroll').classList.remove('fixed-menu')
         }
-    }
+    })
 }
-
-
-
-function IsJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
-
-function paramsToJson(string) {
-    const obj = {};
-    string.split("&").forEach(el => {
-        let newProperty = el.split("=");
-        obj[newProperty[0]] = newProperty[1];
-    });
-    return obj;
-}
-
-
-
-const sum = (a, b) => a + b;
---
+++ src/js/menu.js
@@ -0,0 +1,3 @@
+export function menuScroll() {
+    console.log(123);
+}
